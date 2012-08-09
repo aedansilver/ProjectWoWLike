7323f81138ca9f2d91a45bfb4b4af58fc23432b8
ak47sigh <alexei_93@yahoo.com>
2012-08-09 13:03:15 +0300

    added update weapon skills function, added buff function

--- Trinity/Scripts/main_teleporter.cpp
+++ Trinity/Scripts/main_teleporter.cpp
@@ -14,15 +14,18 @@ enum eEnums
 };
 
 #define MSG_ON_TELEPORT		 "|cff288C00Changing location.. please wait|r"
+
 #define GOSSIP_INT_AUCTION	 "Int'l Auction Function"
+#define GOSSIP_INT_EXPLORE	 "Int'l Explore All Maps"
+#define GOSSIP_INT_FORTUNE	 "|cffAE1EBEDarkness Aura of Fortune|r"
+#define GOSSIP_INT_MAXSKILL	 "|cffC93325Add Your Skill to Max|r"
+
 #define GOSSIP_ITEM_RETURN   "|cff565656Return|r"
 #define GOSSIP_ITEM_MAINMENU "|cff565656[Main Menu]|r"
-#define GOSSIP_ITEM_1        "Global GPS Teleport"
+#define GOSSIP_ITEM_GLOBAL   "Global GPS Teleport"
+
 #define GOSSIP_WORLD_CITY    "|cff288C00 [World Main City]|r"
 #define GOSSIP_WORLD_BIRTH   " [World Birthplace]"
-#define GOSSIP_ITEM_4        "Exit"
-#define GOSSIP_ITEM_5        "Exit"
-#define GOSSIP_ITEM_6        "Exit"
 
 class gossip_teleporter : public CreatureScript
 {
@@ -35,42 +38,44 @@ class gossip_teleporter : public CreatureScript
 
         bool OnGossipHello(Player* player, Creature* creature)
         {
-				player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, GOSSIP_ITEM_1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+2);
-				player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, GOSSIP_ITEM_4, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+4);
+				player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, GOSSIP_ITEM_GLOBAL, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+2);
 			switch (player->getClass())
 			{
 				case CLASS_WARRIOR:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Warrior Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Warrior Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_ROGUE:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Rogue Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Rogue Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_PALADIN:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Paladin Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Paladin Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_MAGE:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Mage Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Mage Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_WARLOCK:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Warlock Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Warlock Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_SHAMAN:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Shaman Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Shaman Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_HUNTER:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Hunter Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Hunter Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_DRUID:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Druid Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Druid Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_PRIEST:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Priest Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Priest Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 				case CLASS_DEATH_KNIGHT:
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "|cff832E21Death Knight Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "|cff832E21Death Knight Train Skill", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7000);
 					break;
 			}
-			player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, GOSSIP_INT_AUCTION, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7001);
+			player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_INT_AUCTION, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7001);
+			player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_INT_EXPLORE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7002);
+			player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_INT_FORTUNE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7003);
+			player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_INT_MAXSKILL, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7004);
             player->PlayerTalkClass->SendGossipMenu(5695, creature->GetGUID());
 
             return true;
@@ -282,6 +287,48 @@ class gossip_teleporter : public CreatureScript
 			case GOSSIP_ACTION_INFO_DEF + 7001: // Int'l Auction Function
 				{
 					player->GetSession()->SendAuctionHello(guid, creature);
+					break;
+				}
+			case GOSSIP_ACTION_INFO_DEF + 7002: // Reveal All Maps
+				{
+					player->MonsterWhisper("|cff951BA3All maps are now OPEN.|r", player->GetGUID());
+					for (uint8 i = 0; i < PLAYER_EXPLORED_ZONES_SIZE; ++i)
+						player->SetFlag(PLAYER_EXPLORED_ZONES_1+i, 0xFFFFFFFF); // use player->SetFlag(PLAYER_EXPLORED_ZONES_1+i, 0); if unreveal
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "All Maps Open!", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+					player->PlayerTalkClass->SendGossipMenu(5695, creature->GetGUID());
+					break;
+				}
+			case GOSSIP_ACTION_INFO_DEF + 7003: // Fortune Buffs
+				{
+					creature->CastSpell(player, 23736, true); // target, spellid, triggered-e
+						creature->CastSpell(player, 23767, true);
+							creature->CastSpell(player, 23768, true);
+								creature->CastSpell(player, 23766, true);
+									creature->CastSpell(player, 23769, true);
+								creature->CastSpell(player, 23738, true);
+							creature->CastSpell(player, 23737, true);
+						creature->CastSpell(player, 23735, true);
+						player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Dark Fortune Aura Bless!", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+					player->PlayerTalkClass->SendGossipMenu(5695, creature->GetGUID());
+					break;
+				}
+				case GOSSIP_ACTION_INFO_DEF + 7004: // All skills to max
+				{
+					player->SetSkill(43, player->GetSkillStep(43), player->GetMaxSkillValue(43), player->GetMaxSkillValue(43));
+					player->SetSkill(44, player->GetSkillStep(44), player->GetMaxSkillValue(44), player->GetMaxSkillValue(44));
+					player->SetSkill(45, player->GetSkillStep(45), player->GetMaxSkillValue(45), player->GetMaxSkillValue(45));
+					player->SetSkill(46, player->GetSkillStep(46), player->GetMaxSkillValue(46), player->GetMaxSkillValue(46));
+					player->SetSkill(54, player->GetSkillStep(54), player->GetMaxSkillValue(54), player->GetMaxSkillValue(54));
+					player->SetSkill(55, player->GetSkillStep(55), player->GetMaxSkillValue(55), player->GetMaxSkillValue(55));
+					player->SetSkill(136, player->GetSkillStep(136), player->GetMaxSkillValue(136), player->GetMaxSkillValue(136));
+					player->SetSkill(160, player->GetSkillStep(160), player->GetMaxSkillValue(160), player->GetMaxSkillValue(160));
+					player->SetSkill(173, player->GetSkillStep(173), player->GetMaxSkillValue(173), player->GetMaxSkillValue(173));
+					player->SetSkill(176, player->GetSkillStep(176), player->GetMaxSkillValue(176), player->GetMaxSkillValue(176));
+					player->SetSkill(172, player->GetSkillStep(43), player->GetMaxSkillValue(172), player->GetMaxSkillValue(172));
+					player->SetSkill(226, player->GetSkillStep(226), player->GetMaxSkillValue(226), player->GetMaxSkillValue(226));
+					player->SetSkill(229, player->GetSkillStep(229), player->GetMaxSkillValue(229), player->GetMaxSkillValue(229));
+					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "All Skill Max!", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+					player->PlayerTalkClass->SendGossipMenu(5695, creature->GetGUID());
 					break;
 				}
 				player->CLOSE_GOSSIP_MENU();

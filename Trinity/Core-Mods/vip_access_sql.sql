1f469a0beead1fb87f8717580f8d87c6cc9dc474
ak47sigh <alexei_93@yahoo.com>
2012-08-19 12:43:43 +0300

    Update Trinity/Core-Mods/vip_access_sql.sql

--- Trinity/Core-Mods/vip_access_sql.sql
+++ Trinity/Core-Mods/vip_access_sql.sql
@@ -1,11 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.5.9-log - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL version:             7.0.0.4170
--- Date/time:                    2012-08-19 12:41:40
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
